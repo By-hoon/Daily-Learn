992dc426f8410b53d90289ef9ca03d8a3bc75528
연훈 배 <sbaru1997@naver.com>
2021-01-22 02:56:21 +0900

    + 1011

--- 1~10000/BOJ-1011/1011_Fly me to the Alpha Centauri.cpp
+++ 1~10000/BOJ-1011/1011_Fly me to the Alpha Centauri.cpp
@@ -8,9 +8,22 @@ int main() {
     int X, Y, dis;
     for (int i = 0; i < T; i++) {
         cin >> X >> Y;
-        dis = Y - X - 2;
+        dis = Y - X;
+        int cnt = 1;
+        int cnt2 = 0;
+        int res = 0;
+
         while (dis > 0) {
-            
+            for (int j = 1; j <= cnt; j++) {
+                dis--;
+            }
+            res++;
+            cnt2++;
+            if (cnt2 == 2) {
+               cnt++;
+               cnt2 = 0;
+            }
         }
+        cout << res << '\n';
     }
-} 
+}
