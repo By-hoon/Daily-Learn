e9936cdc5ad2d8f00e8aff9b4912609b90430d47
연훈 배 <sbaru1997@naver.com>
2021-01-23 20:38:41 +0900

    +1929

---
+++ 1~10000/BOJ-1929/1929_소수 구하기.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    int M, N, sqi;
+    cin >> M >> N;
+    for (int i = M; i <= N; i++) {
+        sqi = sqrt(i);
+        if (sqi == 1 && i != 1) {
+            cout << i << '\n';
+            continue;
+        }
+        for (int j = 2; j <= sqi; j++) {
+            if (i % j == 0)
+                break;
+            if (j == (int)sqi) {
+                cout << i << '\n';
+            }
+        }
+        if (i % 2 == 0)
+            continue;
+    }
+}
