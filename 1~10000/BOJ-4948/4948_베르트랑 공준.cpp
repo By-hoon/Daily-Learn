3621b9333d96de0f27e6e0a9c3744194c7d0706e
연훈 배 <sbaru1997@naver.com>
2021-01-24 17:44:52 +0900

    +4948

---
+++ 1~10000/BOJ-4948/4948_베르트랑 공준.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main() {
+    int n, sqi;
+
+    for (;;) {
+        int res = 0;
+        cin >> n;
+        if (n == 0)break;
+        for (int i = n + 1; i <= 2 * n; i++) {
+            sqi = sqrt(i);
+            if (sqi == 1 && i != 1) {
+                res++;
+                continue;
+            }
+            for (int j = 2; j <= sqi; j++) {
+                if (i % j == 0)
+                    break;
+                if (j == (int)sqi) {
+                    res++;
+                }
+            }
+            if (i % 2 == 0)
+                continue;
+        }
+        cout << res << '\n';
+    }
+    system("pause");
+}
