25b4f696c9885fa2e785cce7131e81e8a8a49f88
연훈 배 <sbaru1997@naver.com>
2021-01-25 21:27:18 +0900

    주석추가

--- 1~10000/BOJ-1712/1712_손익분기점.cpp
+++ 1~10000/BOJ-1712/1712_손익분기점.cpp
@@ -2,14 +2,14 @@
 
 using namespace std;
 
-int main(void) {
+int main() {
     int A, B, C;
     int n = 1;
     cin >> A >> B >> C;
-    if (B >= C) {
+    if (B >= C) {          // B가 C보다 크면 -1 출력
         cout << -1;
         return 0;
     }
 
-    cout << A / (C - B) + 1;
+    cout << A / (C - B) + 1; // 나눈값에 +1 하여 출력
 }
