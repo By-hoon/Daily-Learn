babef333b518160a5b401b74f7ca1ac5c09a84c0
By-hoon <76762467+By-hoon@users.noreply.github.com>
2021-01-30 22:22:31 +0900

    Update 3009_네 번째 점.cpp

--- 1~10000/BOJ-3009/3009_네 번째 점.cpp
+++ 1~10000/BOJ-3009/3009_네 번째 점.cpp
@@ -8,11 +8,11 @@ int main() {
 	for (int i = 0; i < 3; i++) {
 		cin >> x[i] >> y[i];
 	}
-	if (x[0] == x[1])resx = x[2];
+	if (x[0] == x[1])resx = x[2];		//짝이 없는 x값을 찾는 과정
 	else if (x[0] == x[2])resx = x[1];
 	else resx = x[0];
 
-	if (y[0] == y[1])resy = y[2];
+	if (y[0] == y[1])resy = y[2];		//짝이 없는 y값을 찾는 
 	else if (y[0] == y[2])resy = y[1];
 	else resy = y[0];
 
