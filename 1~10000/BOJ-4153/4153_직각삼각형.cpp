b36b9ffbb3d99fd0d330c6ff1165076a1cbeb550
By-hoon <76762467+By-hoon@users.noreply.github.com>
2021-01-31 21:59:18 +0900

    Update 4153_직각삼각형.cpp

--- 1~10000/BOJ-4153/4153_직각삼각형.cpp
+++ 1~10000/BOJ-4153/4153_직각삼각형.cpp
@@ -7,9 +7,9 @@ int main() {
 	int x, y, z;
 	for (;;) {
 		cin >> x >> y >> z;
-		if (x + y + z == 0)break;
-		int M = max(max(x, y), max(x, z));
-		if ((pow(M, 2) * 2) == (pow(x, 2) + pow(y, 2) + pow(z, 2)))
+		if (x + y + z == 0)break; 		// 입력이 0,0,0인지 확인하여 종료
+		int M = max(max(x, y), max(x, z));	// 제일 큰 값을 M으로 지정
+		if ((pow(M, 2) * 2) == (pow(x, 2) + pow(y, 2) + pow(z, 2)))   // 빗변제곱의*2 = 삼각형 각 변 제곱의 합 
 			cout << "right" << '\n';
 		else cout << "wrong" << '\n';
 	}
