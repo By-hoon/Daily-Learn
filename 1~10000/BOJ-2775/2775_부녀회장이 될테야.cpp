aa1a0aadf4cc7cd1f85774670cc871b1f2e3f660
By-hoon <76762467+By-hoon@users.noreply.github.com>
2021-02-06 15:51:48 +0900

    주석추가

--- 1~10000/BOJ-2775/2775_부녀회장이 될테야.cpp
+++ 1~10000/BOJ-2775/2775_부녀회장이 될테야.cpp
@@ -3,11 +3,11 @@
 using namespace std;
 
 int run(int a, int b) {
-    if (a == 0)
+    if (a == 0)         //0층일 경우 호 리턴
         return b;
-    if (b == 1)
-        return 1;
-    return (run(a - 1, b) + run(a, b - 1));
+    if (b == 1)         //1호일 경우 1명 리턴
+        return 1;   
+    return (run(a - 1, b) + run(a, b - 1)); //재귀
 }
 
 int main() {
