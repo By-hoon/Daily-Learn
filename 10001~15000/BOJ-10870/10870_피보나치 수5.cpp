ae81071caf543fec3ab79b129619ce1067b25942
By-hoon <76762467+By-hoon@users.noreply.github.com>
2021-02-13 20:22:32 +0900

    Update 10870_피보나치 수5.cpp

--- 10001~15000/BOJ-10870/10870_피보나치 수5.cpp
+++ 10001~15000/BOJ-10870/10870_피보나치 수5.cpp
@@ -3,9 +3,9 @@
 using namespace std;
 
 int run(int num) {
-	if (num == 0)return 0;
-	else if (num == 1)return 1;
-	else return run(num-2) + run(num-1);
+	if (num == 0)return 0;			//0일때 0리턴
+	else if (num == 1)return 1;		//1일때 1리턴
+	else return run(num-2) + run(num-1);	//재귀로 해당 순서까지 구한다.
 }
 
 int main() {
