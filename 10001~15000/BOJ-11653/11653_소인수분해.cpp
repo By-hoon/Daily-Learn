77c2a7d4a9654eea0aa964471ed33b46d4a573aa
By-hoon <76762467+By-hoon@users.noreply.github.com>
2021-02-14 23:51:45 +0900

    Update 11653_소인수분해.cpp

--- 10001~15000/BOJ-11653/11653_소인수분해.cpp
+++ 10001~15000/BOJ-11653/11653_소인수분해.cpp
@@ -6,9 +6,9 @@ using namespace std;
 int main() {
 	int N;
 	cin >> N;
-    for (int i = 2; i <= sqrt(N); i++)
+    for (int i = 2; i <= sqrt(N); i++)	 //sqrt로 제곱근 계산
     {
-        while (N % i == 0)
+        while (N % i == 0)		// i로 딱 나눠 떨어지면 
         {
             cout << i << "\n";
             N = N / i;
@@ -17,5 +17,4 @@ int main() {
 
     if (N > 1)
         cout << N << "\n";
-	system("pause");
 }
