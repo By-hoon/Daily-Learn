98ca4b771f724fbf724ba9b3503c85be9d3e2871
연훈 배 <sbaru1997@naver.com>
2021-02-23 17:35:55 +0900

    +10989

--- 10001~15000/BOJ-10989/10989_수 정렬하기3.cpp
+++ 10001~15000/BOJ-10989/10989_수 정렬하기3.cpp
@@ -8,18 +8,18 @@ int main()
     cin.tie(NULL);
     cout.tie(NULL);
 
-    int T;
-    cin >> T;
+    int N;
+    cin >> N;
 
     int arr[10001] = { 0 };
 
-    for (int i = 0; i < T; i++) {           // 개수 카운트
-        int a;
-        cin >> a;
-        arr[a] += 1;
+    for (int i = 0; i < N; i++) {           // 개수 카운트
+        int num;
+        cin >> num;
+        arr[num] += 1;
     }
 
     for (int i = 1; i <= 10000; i++)        // 개수만큼 출력
         for (int j = 0; j < arr[i]; j++)
             cout << i << "\n";
-}
+}       //계수정렬
