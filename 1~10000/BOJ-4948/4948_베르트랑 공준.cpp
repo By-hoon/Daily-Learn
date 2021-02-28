4ace4b441a8bac0dab88f89ce694f5346641d662
By-hoon <76762467+By-hoon@users.noreply.github.com>
2021-02-28 19:04:32 +0900

    Update 4948_베르트랑 공준.cpp

--- 1~10000/BOJ-4948/4948_베르트랑 공준.cpp
+++ 1~10000/BOJ-4948/4948_베르트랑 공준.cpp
@@ -10,14 +10,14 @@ int main() {
         int res = 0;
         cin >> n;
         if (n == 0)break;
-        for (int i = n + 1; i <= 2 * n; i++) {
+        for (int i = n + 1; i <= 2 * n; i++) {      //n+1 => n보다 크고, 2*n => 2n보다 작고
             sqi = sqrt(i);
-            if (sqi == 1 && i != 1) {
+            if (sqi == 1 && i != 1) {               //2또는 3일 경우
                 res++;
                 continue;
             }
             for (int j = 2; j <= sqi; j++) {
-                if (i % j == 0)
+                if (i % j == 0)                     //홀수일 경우
                     break;
                 if (j == (int)sqi) {
                     res++;
