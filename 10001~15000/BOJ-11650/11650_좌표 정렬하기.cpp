d17372348a3d36d05f3a35cf345e17cf65f7b96b
연훈 배 <sbaru1997@naver.com>
2021-03-09 20:08:02 +0900

    +11650

--- 10001~15000/BOJ-11650/11650_좌표 정렬하기.cpp
+++ 10001~15000/BOJ-11650/11650_좌표 정렬하기.cpp
@@ -9,5 +9,14 @@ int main()
 {
 	int N;
 	cin >> N;
-	vector<pair<int, int>> 
+	vector<pair<int, int>> arr;
+	pair<int, int> arr2;
+	for (int i = 0; i < N; i++)
+	{
+		cin >> arr2.first >> arr2.second;
+		arr.push_back(arr2);
+	}
+	sort(arr.begin(), arr.end());
+	for (int i = 0; i < N; i++)
+		cout << arr[i].first << ' ' << arr[i].second << '\n';
 }
