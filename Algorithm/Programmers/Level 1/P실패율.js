6f176e09e6805f9b55ee9595566b3c99738ebed3
연훈 배 <sbaru1997@naver.com>
2021-07-01 14:34:25 +0900

    finish

--- Algorithm/Programmers/Level 1/P실패율.js
+++ Algorithm/Programmers/Level 1/P실패율.js
@@ -2,11 +2,14 @@ function solution(N, stages) {
     var staging = [];
     var player = stages.length, cnt = 1;
     var fail = [];
+
+    //각 스테이지에 몇 명 있는지 확인
     stages.forEach(i => {
         if(!staging[i])staging[i] = 0
         if(i<=N)staging[i]++;
     });
 
+    //각 스테이지 별 실패율과 스테이지 값을 저장
     while(cnt < (N+1)){
         if(!staging[cnt])staging[cnt] = 0;
         var divide = (staging[cnt] / player);
@@ -16,14 +19,20 @@ function solution(N, stages) {
         };
         fail.push(info);
         player -= staging[cnt];
-        console.log(player);
         cnt++;
     };
-    console.log(fail);
+
+    //실패율 내림차순 정렬
+    fail.sort(function(a,b){
+        if(a.failure_Rate < b.failure_Rate)return 1;
+        if(a.failure_Rate > b.failure_Rate)return -1;
+        return 0;
+    })
     var answer = [];
+
+    //실패율의 내림차순으로 인덱스 저장
+    fail.forEach(i => {
+        answer.push(i.index);
+    });
     return answer;
 }
-
-const N = 5;
-const stages = [2, 1, 2, 6, 2, 4, 3, 3];
-solution(N, stages);
