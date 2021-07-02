7cd97712e5bfc074a9c4606198a97640c9cef0ea
연훈 배 <sbaru1997@naver.com>
2021-07-03 05:44:13 +0900

    finish

--- Algorithm/Programmers/Level 1/P[1차]다트 게임.js
+++ Algorithm/Programmers/Level 1/P[1차]다트 게임.js
@@ -1,10 +1,34 @@
 function solution(dartResult) {
     const arr = dartResult.split('');
+    var score = [], bonus =[], option=[];
+    var answer = 0, cnt = -1, cal = 0, chk = 0;
     arr.forEach(i => {
-        if(!isNaN(i))console.log(i);
+        if(!isNaN(i)){
+            if(chk !== 0 && i === '0'){
+                score.pop();
+                score.push('10');
+                chk = 0;
+            }
+            else {
+                score.push(i);
+                chk ++;
+            }
+        }
+        else if(i !== "*" && i !== "#")
+        {
+            (i === "S") ? bonus.push(1) : (i === "D") ? bonus.push(2) : bonus.push(3);
+            option.push(1);
+            cnt++;
+            chk = 0;
+        }
+        else
+            (i === "*") ? option[cnt] = 2 : (i === "#") ? option[cnt]=-1 : null;
+        
     });
-    var answer = 0;
+    option.push(0);
+    for(let i=0; i<3; i++){
+        cal = (Math.pow(score[i], bonus[i]) * option[i])
+        answer += ((option[i+1] === 2) ? cal * 2 : cal);
+    }
     return answer;
 };
-
-solution('1S2D*3T');
