3dc9d0e3581c5e02cafc8c47fc979144745e2310
연훈 배 <sbaru1997@naver.com>
2021-08-14 23:11:00 +0900

    finish

--- Algorithm/Programmers/Level2/P게임 맵 최단거리.js
+++ Algorithm/Programmers/Level2/P게임 맵 최단거리.js
@@ -1,26 +1,27 @@
 function solution(maps) {
-    // 남서북동 순서
-    const dy = [1, 0, -1, 0];
-    const dx = [0, -1, 0, 1];
-    const row = maps.length; // 행
-    const col = maps[0].length; // 열
-    const visitCount = [...maps].map((r) => r.map((r) => 1));
+  const dy = [1, 0, -1, 0];
+  const dx = [0, -1, 0, 1];
+  const row = maps.length;
+  const col = maps[0].length;
+  const mapCount = [...maps].map((r) => r.map((r) => 1));
 
-    const queue = [[0, 0]];
-    while (queue.length) {
-      const [y, x] = queue.shift();
-  
-      for (let k = 0; k < 4; k++) {
-        const ny = y + dy[k];
-        const nx = x + dx[k];
-  
-        if (ny >= 0 && nx >= 0 && ny < row && nx < col) {
-          if (maps[ny][nx] === 1 && visitCount[ny][nx] === 1) {
-            visitCount[ny][nx] = visitCount[y][x] + 1;
-            queue.push([ny, nx]);
-          }
+  const queue = [[0, 0]];
+  while (queue.length) {
+    const [y, x] = queue.shift();
+    for (let k = 0; k < 4; k++) {
+      const ny = y + dy[k];
+      const nx = x + dx[k];
+
+      if (ny >= 0 && nx >= 0 && ny < row && nx < col) {
+        if (maps[ny][nx] === 1 && mapCount[ny][nx] === 1) {
+          mapCount[ny][nx] = mapCount[y][x] + 1;
+          queue.push([ny, nx]);
         }
       }
     }
-    return visitCount[row - 1][col - 1] === 1 ? -1 : visitCount[row - 1][col - 1];
   }
+  return mapCount[row - 1][col - 1] === 1 ? -1 : mapCount[row - 1][col - 1];
+}
+
+const maps = [[1, 0, 1, 1, 1], [1, 0, 1, 0, 1], [1, 0, 1, 1, 1], [1, 1, 1, 0, 1], [0, 0, 0, 0, 1]];
+solution(maps);
