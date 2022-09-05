ceca9879e763464af3ec504255b80d240df10e47
Yeunhun Bae <sbaru1997@naver.com>
2022-09-05 21:10:33 +0900

    finish

--- Algorithm/Programmers/Level 3/P베스트앨범.js
+++ Algorithm/Programmers/Level 3/P베스트앨범.js
@@ -1,41 +1,34 @@
 function solution(genres, plays) {
   let answer = [];
+  const songs = {};
   const combination = {};
-  genres.forEach((genre, index) => {
-    const play = plays[index];
-    if (!combination[genre]) combination[genre] = { totalPlay: 0, songs: [] };
-    combination[genre].totalPlay += play;
-    if (combination[genre].songs.length < 2) {
-      combination[genre].songs.push({
-        index,
-        plays: play,
-      });
-      if (combination[genre].songs.length === 2) combination[genre].songs.sort((a, b) => b.plays - a.plays);
-    } else {
-      if (combination[genre].songs[0].plays < play) {
-        combination[genre].songs.pop();
-        combination[genre].songs.unshift({
-          index,
-          plays: play,
-        });
-      } else if (combination[genre].songs[0].plays > play && combination[genre].songs[1].plays < play) {
-        combination[genre].songs.pop();
-        combination[genre].songs.push({
-          index,
-          plays: play,
-        });
-      }
+  genres.forEach((genre, idx) => {
+    if (!songs[genre]) songs[genre] = [];
+    if (combination[genre] == null || combination[genre] == undefined) {
+      combination[genre] = 0;
     }
+    const info = {
+      genre,
+      play: plays[idx],
+      index: idx,
+    };
+    songs[genre].push(info);
+    combination[genre] += plays[idx];
   });
-  const newGenres = [];
-  Object.keys(combination).forEach((key) => {
-    newGenres.push([key, combination[key].totalPlay]);
-  });
-  newGenres
-    .sort((a, b) => b[1] - a[1])
-    .forEach((newGenre) => {
-      answer.push(combination[newGenre[0]].songs[0].index);
-      answer.push(combination[newGenre[0]].songs[1].index);
+
+  const combinationArr = [];
+  for (let key in combination) {
+    combinationArr.push({
+      genre: key,
+      play: combination[key],
     });
+  }
+  combinationArr.sort((a, b) => b.play - a.play);
+  combinationArr.forEach((el) => {
+    const targetGenre = songs[el.genre];
+    targetGenre.sort((a, b) => b.play - a.play);
+    targetGenre[0] && answer.push(targetGenre[0].index);
+    targetGenre[1] && answer.push(targetGenre[1].index);
+  });
   return answer;
 }
